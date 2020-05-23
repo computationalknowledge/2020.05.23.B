1f3a2cefbc9ae8088cc36da37f3f070b4a44f630
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-05-23 14:54:54 -0400

    2
    2

--- ClassCode1.cs
+++ ClassCode1.cs
@@ -7,13 +7,22 @@ namespace ConsoleApp22
     {
         static void Main(string[] args)
         {
-
+            // remember this is version 1
+            // next week we will web enable this to take input from the HTML Page
+            // and store data in a MS SQL database
+            public Task task1 = new Task('2018,7,1', "Toronto", new Activity());
         }
     }
 
     public class Task
     {
-        public DateTime dtStamp;
+        public Task(DateTime aDateTime, String aLocaion, Activity activ1)
+    {
+        this.dtStamp = aDateTime;
+        this.Location = aLocaion;
+        this.anActivity = activ1;
+    }
+    public DateTime dtStamp;
         public String Location;
         public Activity anActivity;
 
@@ -21,6 +30,11 @@ namespace ConsoleApp22
 
     public class Activity
     {
+        public Activity(String descrip, Double aCost)
+    {
+        this.Description = descrip;
+        this.cost = aCost;
+    }
         public String Description;
         public Double cost;
     }
