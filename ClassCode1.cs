dba455f6b036ffe839d2f642aec3fc67d2075ba0
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-05-23 14:42:05 -0400

    1
    1

---
+++ ClassCode1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Task
+    {
+        public DateTime dtStamp;
+        public String Location;
+        public Activity anActivity;
+
+    }
+
+    public class Activity
+    {
+        public String Description;
+        public Double cost;
+    }
+
+    public class Records
+    {
+        Queue DataStructure1;
+    }
+}
